2114f6716cf9aa91259131df9b6b8f0877bc6fe8
SonHeoMin <88872973+SonHeoMin@users.noreply.github.com>
2021-08-13 13:31:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Bú Cu Suusussussusu',
+    introDesc: `Địt Mẹ Mày
+    Phong Bố M Là Sơn đây`,
+
+    
+
+    btnIntro: 'Địt Mẹ Mày',
+    title: 'M có Bị Gay Ko',
+    desc: 'M bị Gay Thật À VL ',
+    btnYes: 'Uk T bị gay',
+    btnNo: 'Gay Con Cặc',
+    question:'M bị Gay Thật Rồi Tao Biết Mà Tí T chó Bú cu suusu',
+    btnReply: 'Ấn Vào Đây Củ Cặc',
+    reply: 'Uk Tao Gay',
+    mess: 'Địt mẹ mày biết đéo gì về Flex.Gay Lỏd',
+    messDesc: 'Bú cu suusususu.',
+    btnAccept: 'Okiiiii lun:33',
+    messLink: 'http://SonDzandPhongGay' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
