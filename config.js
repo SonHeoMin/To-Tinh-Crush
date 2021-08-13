0d1a9c127928951b196eca6083bb57902238bf79
SonHeoMin <88872973+SonHeoMin@users.noreply.github.com>
2021-08-13 13:15:09 +0700

    Update config.js
    Bruh

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Địt mẹ mày Phong',
+    introDesc: `Bú Cu Su Su Su Su Su`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'Mày Là Gay À',
+    desc: 'Mày Bị Gay Cmnr ',
+    btnYes: 'Uk Tao Gay',
+    btnNo: 'Không,Tao Đéo Gay',
+    question:'Địt mẹ m sao m lại gay r còn đòi bú cu t',
+    btnReply: 'Bấm Vào Đây Thằng Cặc',
+    reply: 'Bố Mày Gay Đéo Đâu Địt Mẹ',
+    mess: 'Tao Biết Mày Gay mà. Dơ cu ra cho t bú',
+    messDesc: 'Sếch Sếch Sếch Phờ Ri Sếch Tô Nai.',
+    btnAccept: 'Địt mẹ mày cút',
+    messLink: 'https://m.facebook.com/messages/?entrypoint=jewel&no_hist=1'
 }
